1377b13647aefbae46879048d871a7f60879a423
almar54 <almarosette54@gmail.com>
2023-09-19 18:49:34 +0300

    middle of log in

--- Abstract_Robots/WndLogin.xaml.cs
+++ Abstract_Robots/WndLogin.xaml.cs
@@ -28,29 +28,53 @@ namespace Robots_inc
         {
             InitializeComponent();
             GetSixWorkers();
+            GetEightRob();
+            GetFiveTasks();
+        }
+
+        private List<Mission> GetFiveTasks()
+        {
+            List<Mission> tasks = new List<Mission>();
+            tasks.Add(new Mission(DateTime.Now, "fire"));
+            tasks.Add(new Mission(DateTime.Now, "update task"));
+            tasks.Add(new Mission(DateTime.Now, "new robot"));
+            tasks.Add(new Mission(DateTime.Now, "retire robot"));
+            tasks.Add(new Mission(DateTime.Now, "add worker"));
+            return tasks;
+        }
+
+        private List<RobotSpy> GetEightRob()
+        {
+            List<RobotSpy> robots = new List<RobotSpy>();
+            robots.Add(new RobotQuad());
+            robots.Add(new RobotFly());
+            robots.Add(new RobotQuad());
+            robots.Add(new RobotQuad());
+            robots.Add(new RobotFly());
+            robots.Add(new RobotQuad());
+            robots.Add(new RobotQuad());
+            robots.Add(new RobotWheels());
+            return robots;
         }
 
         private List<Worker> GetSixWorkers()
         {
             List<Worker> workers = new List<Worker>();
-
+            workers.Add(new GeneralManager(25, "roy", "123", DateTime.Now, "123", 15000));
+            workers.Add(new OperationManager(50, 8, 3, "jade", "456", DateTime.Now, "456"));
+            workers.Add(new OperationManager(50, 8, 3, "ben", "225", DateTime.Now, "225"));
+            workers.Add(new OperationalWorker(50, 8, "john", "826", DateTime.Now, "826"));
+            workers.Add(new OperationalWorker(50, 8, "ron", "334", DateTime.Now, "334"));
+            workers.Add(new OperationalWorker(50, 8, "nir", "555", DateTime.Now, "555"));
+            return workers;
         }
+
 
         private void btnClose_Click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("Have a robotic day (-:","Good bye",MessageBoxButton.OK);
             this.Close();
         }
-
-
-        //משימה 1
-        // : כתבו פעולה המחזירה אוסף של 6 עובדים 
-        //מנכ"ל אחד, 2 מנהלי תפעול ו-3 עובדי תפעול
-        //כתבו זימון לפעולה שכתבתם בפעולה הבונה של החלון
-
-        //משימה 2
-        //כתבו פעולה המחזירה אוסף של 8 רובוטים
-        //כתבו זימון לפעולה שכתבתם בפעולה הבונה של החלון
 
         //משימה 3
         //כתבו פעולה המחזירה אוסף של 5 משימות
